b517000c010c4a6d522d5c7fa853f12be84f6b72
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2023-01-08 18:33:19 +0300

    init
    start frontend

---
+++ APIReactProject/apireactproject/src/App.js
@@ -0,0 +1,18 @@
+﻿import React, { useState } from 'react';
+import './styles/app.css';
+import './Components/PostItem';
+import PostItem from './Components/PostItem';
+
+function App() {
+
+    return (
+        <div className="App">
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+        </div>
+        );
+}
+
+export default App;
---
+++ APIReactProject/apireactproject/src/Components/PostItem.jsx
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+
+const PostItem = () => {
+    return (
+        <div className="post">
+            <div className="postContent">
+                <strong>1. JS </strong>
+                <div>
+                    JS - это язык программирования
+                </div>
+            </div>
+            <div className="postButtons">
+                <button>Удалить</button>
+            </div>
+        </div>  
+    );
+}
+
+export default PostItem;
